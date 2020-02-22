b4a4f8083c1893b43404598179d7409e696a1e69
gavin <15195685522@163.com>
2020-02-22 15:34:50 +0800

    第二天

--- one.py
+++ one.py
@@ -1,3 +1,16 @@
-new code
- one demo
-new test demo
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# classmeates = ['Bob','Tracy','Job','Job']
+# print ("First list length : ", len(classmeates))
+# print(classmeates)
+
+L = ['Python','Java',['abc','qwe'],'oracle',123,True]
+print('First L lenth = ',len(L))
+
+# L[1] = 'C++'
+print(L[2][1])
+
+S = (2,)
+print(S)
+
