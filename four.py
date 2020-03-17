9a89262616d1f7bc389eee4bce380461ef028e14
gavin <15195685522@163.com>
2020-03-17 20:34:10 +0800

    test0317

---
+++ four.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# cs = ["1+2+3",'job','mom']
+# for c in cs:
+#     print(c)
+# sum = 0
+# for s in [1, 2, 3, 4, 5, 6, 7, 8, 9, 101]:
+#     sum = sum + s
+# print(sum)
+#
+# sum = 0
+# for s in range(101):
+#     sum =sum + s
+# print(sum)
+#
+# sum1 = 0
+# n  = 99
+# while n > 0:
+#     sum1 = sum1 +n
+#     n =n - 2
+# print(sum1)
+
+sum2 = 0
+s = 100
+while s > 0:           # 当s 大于0
+    sum2 = sum2 + s   # sum2 加上 0
+    s = s - 2        #
+print(sum2)
