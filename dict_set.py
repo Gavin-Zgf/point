b2d411102160e0b9d7a3420057925706675d2e4d
v-zhuguofeng <v-zhuguofeng@momenta.ai>
2020-04-10 15:19:17 +0800

    0410_test

---
+++ dict_set.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# dict：字典 dict全称dictionary，在其他语言中也称为map，使用键-值（key-value）存储，具有极快的查找速度
+
+d = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
+print(d['Michael'])
+
